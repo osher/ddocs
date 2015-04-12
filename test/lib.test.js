dc822b218121f63ca0114b051598ca6e04a3f9eb
osher <osher.filter@gmail.com>
2015-04-12 14:21:01 +0300

    input checks

--- test/lib.test.js
+++ test/lib.test.js
@@ -8,8 +8,52 @@ module.exports =
         SUT.should.be.a.Function;
         SUT.length.should.eql(2)
     }
-  , "when used with valid arguments" : 
-    {
+  , "when used with invalid arguments" : 
+    { "should yield an error" : 
+      function(done) {
+          var cases
+            , count = 0
+            ;
+          cases = 
+            [ null
+            , "str"
+            , 3223
+            , true
+            , false
+            , function() {}
+            , {}
+            , { ddocs : null }
+            , { ddocs : true }
+            , { ddocs : "Str" }
+            , { ddocs : 123 }
+            , { ddocs : [ null ] }
+            , { ddocs : [ true ] } 
+            , { ddocs : [ 'str' ] }
+            , { ddocs : [ 456 ] }
+            , { ddocs : 
+                [ {}
+                ] 
+              }
+            , { ddocs : 
+                [ { name   : "myddoc1"
+                  , baseidr: "./test/fixtures/"
+                  , src    : "_desing"
+                  , host   : "http://localhost:5984/"
+                  , dbs    : [ "bla" ]
+                  }
+                , { name   : "myddoc2"
+                  }
+                ] 
+              }
+            ]
+          cases.forEach(function(args, ix) {
+              SUT(args, function(err) {
+                  Should.exist(err, "no error for case: " + ix);
+
+                  if (++count == cases.length)  done()
+              })
+          })
+      }
     }
   }
 }
