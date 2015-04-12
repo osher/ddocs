25eee3570b1f1038131238516b70c5a1bcbf8607
osher <osher.filter@gmail.com>
2015-04-12 14:29:55 +0300

    pending tests added

--- test/lib.test.js
+++ test/lib.test.js
@@ -8,7 +8,7 @@ module.exports =
         SUT.should.be.a.Function;
         SUT.length.should.eql(2)
     }
-  , "when used with invalid arguments" : 
+  , "when used with invalid options" : 
     { "should yield an error" : 
       function(done) {
           var cases
@@ -36,7 +36,7 @@ module.exports =
               }
             , { ddocs : 
                 [ { name   : "myddoc1"
-                  , baseidr: "./test/fixtures/"
+                  , basedir: "./test/fixtures/"
                   , src    : "_desing"
                   , host   : "http://localhost:5984/"
                   , dbs    : [ "bla" ]
@@ -53,6 +53,18 @@ module.exports =
                   if (++count == cases.length)  done()
               })
           })
+      }
+    }
+  , "when used with valid options" : 
+    { "and all is well (basic run)" : 
+      { "should create one couchapp for every given command" : null
+      , "should push the app to each target db" : null
+      }
+    , "and a cmd contains .att section" : 
+      { "should load the attachments upon creation of the app" : null
+      }
+    , "and a cmd contains .onloaded hook" : 
+      { "should apply the hook upon reation of the app, after loading attachments" : null
       }
     }
   }
